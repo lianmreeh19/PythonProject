9a0dd9dbcbb50ef0549a1c6466cab140c8309533
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-08 08:42:45 +0200

    link_example

---
+++ KeremQA/selenium_example/link_example.py
@@ -0,0 +1,20 @@
+import time
+from time import sleep
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
+
+print("Test start")
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get("https://www.calculator.net/")
+age_link = driver.find_element(By.LINK_TEXT,"Age Calculator")
+age_link.click()
+
+driver.close()
