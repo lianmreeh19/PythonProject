21273c1e05afa9477a57f6a42b2787b45b1dfd4c
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-08 11:19:55 +0200

    ebay_with_reading_text.py

---
+++ KeremQA/selenium_example/ebay_with_reading_text.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from KeremQA.selenium_example.seleniumBaseKerem import seleniumBaseKerem
+
+base = seleniumBaseKerem()
+driver = base.selenium_start()
+
+driver.get("https://www.ebay.com/")
+advanced_button= driver.find_element(By.LINK_TEXT, "Advanced")
+advanced_text = advanced_button.text
+if advanced_text == "Advanced":
+    driver.find_element(By.LINK_TEXT, "Advanced").click()
+    print(f"the URL for this page is {driver.current_url}")
+else:
+    print("it's not the right name of the button you test")
+
+driver.close()
