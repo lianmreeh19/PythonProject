87836d5b00813dfc7c124c02fce646d1c140dfa1
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-11 12:21:50 +0200

    ebay pytest training

---
+++ KeremQA/selenium_example/ebay_pytest.py
@@ -0,0 +1,27 @@
+import unittest
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from KeremQA.selenium_example.seleniumBaseKerem import seleniumBaseKerem
+
+class ebay_pytest(unittest.TestCase):
+    def setUp(self):
+        self.base = seleniumBaseKerem()
+        self.driver = self.base.selenium_start_with_url("https://www.ebay.com/")
+
+    def tearDown(self):
+        self.base.selenium_stop()
+
+    def test_advanced_button(self):
+        advanced_button = self.driver.find_element(By.CLASS_NAME,"gh-search-button__advanced-link").click()
+        url = self.driver.current_url
+
+        assert url == "https://www.ebay.com/sch/ebayadvsearch", "URL should be: https://www.ebay.com/sch/ebayadvsearch"
+        print("Advanced button was clicked")
+
+    def test_find_items(self):
+        advanced_button = self.driver.find_element(By.CLASS_NAME,"gh-search-button__advanced-link").click()
+        input_items = self.driver.find_element(By.ID,"_nkw")
+        input_items.send_keys("Lian")
+        input_items.send_keys(Keys.ENTER)
+        url = self.driver.current_url
+
