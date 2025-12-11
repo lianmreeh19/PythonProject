b2b730f53e14174660d3d62310aaa4f1f8ee0774
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-11 13:41:35 +0200

    ebay pytest training

--- KeremQA/selenium_example/ebay_pytest.py
+++ KeremQA/selenium_example/ebay_pytest.py
@@ -12,16 +12,18 @@ class ebay_pytest(unittest.TestCase):
         self.base.selenium_stop()
 
     def test_advanced_button(self):
-        advanced_button = self.driver.find_element(By.CLASS_NAME,"gh-search-button__advanced-link").click()
+        advanced_button = self.driver.find_element(By.LINK_TEXT,"Advanced").click()
         url = self.driver.current_url
 
         assert url == "https://www.ebay.com/sch/ebayadvsearch", "URL should be: https://www.ebay.com/sch/ebayadvsearch"
         print("Advanced button was clicked")
 
     def test_find_items(self):
-        advanced_button = self.driver.find_element(By.CLASS_NAME,"gh-search-button__advanced-link").click()
+        advanced_button = self.driver.find_element(By.LINK_TEXT,"Advanced").click()
         input_items = self.driver.find_element(By.ID,"_nkw")
         input_items.send_keys("Lian")
         input_items.send_keys(Keys.ENTER)
         url = self.driver.current_url
+        assert "Lian" in url, "URL should contain the word 'Lian'"
+        print(f"Item was found successfully")
 
