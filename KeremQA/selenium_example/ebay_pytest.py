e41c81bd01097df2c2054597458e73f041539373
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-14 11:27:27 +0200

    drop down example with pytest (ebay)

--- KeremQA/selenium_example/ebay_pytest.py
+++ KeremQA/selenium_example/ebay_pytest.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from KeremQA.selenium_example.seleniumBaseKerem import seleniumBaseKerem
 
 class ebay_pytest(unittest.TestCase):
@@ -19,12 +21,24 @@ class ebay_pytest(unittest.TestCase):
         print("Advanced button was clicked")
 
     def test_find_items(self):
-        item = "Lian"
         advanced_button = self.driver.find_element(By.LINK_TEXT,"Advanced").click()
+        item = "Lian"
+        input_items = self.driver.find_element(By.ID,"_nkw")
+        input_items.send_keys(item)
+
+    def test_keyboard_options_dropdown(self):
+        advanced_button = self.driver.find_element(By.LINK_TEXT,"Advanced").click()
+        keyboard_options = Select(self.driver.find_element(By.ID,"s0-1-20-4[0]-7[1]-_in_kw"))
+        keyboard_options.select_by_index(3)
+        item = "Lian"
         input_items = self.driver.find_element(By.ID,"_nkw")
         input_items.send_keys(item)
         input_items.send_keys(Keys.ENTER)
+        time.sleep(3)
         url = self.driver.current_url
         assert item in url, "URL should contain the word 'Lian'"
         print("Item was found successfully")
+
+
+
 
