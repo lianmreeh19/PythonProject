5bfc547092146512cbdaec58d0d5c44928a6cc07
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-16 10:26:30 +0200

    new tests

--- KeremQA/selenium_example/ebay_pytest.py
+++ KeremQA/selenium_example/ebay_pytest.py
@@ -31,13 +31,27 @@ class ebay_pytest(unittest.TestCase):
         keyboard_options = Select(self.driver.find_element(By.ID,"s0-1-20-4[0]-7[1]-_in_kw"))
         keyboard_options.select_by_index(3)
         item = "Lian"
-        input_items = self.driver.find_element(By.ID,"_nkw")
+        input_items = self.driver.find_element(By.ID, "_nkw")
         input_items.send_keys(item)
         input_items.send_keys(Keys.ENTER)
         time.sleep(3)
         url = self.driver.current_url
+
         assert item in url, "URL should contain the word 'Lian'"
         print("Item was found successfully")
+
+    def test_search_including_checkbox(self):
+        advanced_button = self.driver.find_element(By.LINK_TEXT,"Advanced").click()
+        search_including = self.driver.find_element(By.ID,"s0-1-20-5[1]-[0]-LH_TitleDesc").click()
+        time.sleep(3)
+
+    def test_condition_radio_button(self):
+        advanced_button = self.driver.find_element(By.LINK_TEXT,"Advanced").click()
+        time.sleep(3)
+        description_button = self.driver.find_element(By.ID,"s0-1-20-5[1]-[0]-LH_TitleDesc")
+        self.base.click_and_assert_on_element(description_button)
+
+
 
 
 
