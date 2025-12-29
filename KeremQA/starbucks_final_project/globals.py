c432903d72a5c9cd2f7e8dbd3ce2b1f64c296a67
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-29 11:54:21 +0200

    globals starbucks second commit

--- KeremQA/starbucks_final_project/globals.py
+++ KeremQA/starbucks_final_project/globals.py
@@ -1,2 +1,6 @@
 BASE_URL = "https://www.starbucks.com/"
 START_AN_ORDER_BUTTON = "Start an order"
+ESPRESSO_TYPE = "Blonde Espresso"
+EXPECTED_BUTTONS_LIST = ["MENU", "REWARDS", "GIFT CARDS"]
+PROTEIN_CATEGORY = "Protein"
+NEW_YORK_STORE = "New York"
