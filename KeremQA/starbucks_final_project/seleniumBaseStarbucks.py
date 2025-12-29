610c3d063227007bcc5b180f1f084d99c49faa2f
lianmreeh19 <lianmreeh19@gmail.com>
2025-12-29 08:05:18 +0200

    commit for the selenium base of my project (starbucks)

---
+++ KeremQA/starbucks_final_project/seleniumBaseStarbucks.py
@@ -0,0 +1,25 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+class seleniumBaseStarbucks():
+
+    def selenium_start_with_url(self, url):
+        print("test start")
+        # service = ChromeService(executable_path=ChromeDriverManager().install())
+        options = Options()
+        options.add_argument("--incognito")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        # self.driver = webdriver.Chrome(service=service)
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.driver.get(url)
+        return self.driver
+
+    def selenium_stop(self):
+        print("test stop")
+        self.driver.close()
